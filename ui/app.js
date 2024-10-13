ce73bab1c4c40965f8d236f719a93fbcf9b446f4
atmask <benjaminajm@gmail.com>
2024-10-12 22:38:15 -0400

    Move to tailwind

--- ui/app.js
+++ ui/app.js
@@ -197,77 +197,6 @@ function handleTrackEvent(e) {
     addRemoteStream(e.streams[0]);
 }
 
-
-
-//async function joinSession() {
-//    const name = document.getElementById('name').value;
-//    if (!name) {
-//        alert('Please enter your name');
-//        return;
-//    }
-//
-//    document.getElementById('join-screen').style.display = 'none';
-//    document.getElementById('participant-view').style.display = 'block';
-//
-//    peerConnection = new RTCPeerConnection({
-//        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
-//    });
-//
-//    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-//    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
-//
-//    const localVideo = document.createElement('video');
-//    localVideo.srcObject = localStream;
-//    localVideo.autoplay = true;
-//    localVideo.muted = true;
-//    document.getElementById('videos').appendChild(localVideo);
-//
-//    ws = new WebSocket(`ws://${window.location.host}/ws`);
-//
-//    ws.onopen = () => {
-//        console.log('Connected to the signaling server');
-//        ws.send(JSON.stringify({ type: 'join', name: name }));
-//        createOffer();
-//    };
-//
-//    ws.onmessage = async (message) => {
-//        const data = JSON.parse(message.data);
-//        switch (data.type) {
-//            case 'offer':
-//                console.log('Received offer');
-//                await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
-//                const answer = await peerConnection.createAnswer();
-//                await peerConnection.setLocalDescription(answer);
-//                ws.send(JSON.stringify({ type: 'answer', answer: answer }));
-//                break;
-//            case 'answer':
-//                console.log('Received answer');
-//                await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
-//                break;
-//            case 'candidate':
-//                console.log('Received ICE candidate');
-//                await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
-//                break;
-//            default:
-//                console.log('Unknown message type');
-//                break;
-//        }
-//    };
-//
-//
-//    peerConnection.onicecandidate = (event) => {
-//        if (event.candidate) {
-//            ws.send(JSON.stringify({ type: 'candidate', candidate: event.candidate }));
-//        }
-//    };
-//
-//    peerConnection.ontrack = (event) => {
-//        console.log('Adding a new stream');
-//        addRemoteStream(event.streams[0]);
-//    };
-//
-//}
-//
 function toggleMute() {
     localStream.getAudioTracks().forEach(track => track.enabled = !track.enabled);
     isMuted = !isMuted;
@@ -281,6 +210,7 @@ function toggleVideo() {
 }
 
 function addRemoteStream(stream) {
+    console.log("Adding Remote Stream");
     const remoteVideo = document.createElement('video');
     remoteVideo.srcObject = stream;
     remoteVideo.autoplay = true;
